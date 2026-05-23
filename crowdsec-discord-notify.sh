fe01496c500d3629c55c640d728b380fafe95704
LuanBertozzi7 <LuanBertozzi7@hotmail.com>
2026-05-23 13:24:06 -0400

    chore: removed scenario

--- crowdsec-discord-notify.sh
+++ crowdsec-discord-notify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOG_FILE="/var/log/crowdsec.log"
-DISCORD_WEBHOOK="" # your webhook
+DISCORD_WEBHOOK="your webhook"
 LOG_COUNT=0 # Count all the CrowdSec logs.
 echo "Starting: $LOG_FILE"
 
@@ -9,10 +9,10 @@ tail -f "$LOG_FILE" | while read -r line; do
   if echo "$line" | grep -q "module=db" && echo "$line" | grep -q "ban on"; then
     IP=$(echo "$line" | grep -oP 'ban on (Ip|Range) \K[\d./]+')
     COUNTRY=$(echo "$line" | grep -oP 'by ip [\d.]+ \(\K[^)]+')
-    SCENARIO=$(echo "$line" | grep -oP 'crowdsecurity/[\w-]+')
+    # scenario removed
     DURATION=$(echo "$line" | grep -oP '[\w]+(?= ban on)')
 
-    MSG="**CrowdSec Ban Info** \nIP: $IP ($COUNTRY)\nCenário: $SCENARIO\nDuração: $DURATION\nRegistros: $LOG_COUNT"
+    MSG="**CrowdSec Ban Info** \nIP: $IP ($COUNTRY) \nDuração: $DURATION\nRegistros: $LOG_COUNT"
 
       curl -s -X POST "$DISCORD_WEBHOOK" \
         -H "Content-Type: application/json" \
